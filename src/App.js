58fd9cce5f242d56e9f08de04ce0a472fef965a1
Moova Shaka <mrmoovashaka@gmail.com>
2021-05-03 15:02:00 +0100

    holding page setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './swllogo6.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome To SWL Garden Structures our new site is coming Soon!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://swlgardenstructures.co.uk"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
+        <p>
+        In the meantime you can Contact us on: 07896 043098 or 07738 261886
+        </p>
       </header>
     </div>
   );
